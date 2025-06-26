1d5dc387f5528842f6dbdc34c3b696f3669306f6
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-06-25 20:39:54 -0400

    Footer Section

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './footer.css';
+import { FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <div className='footer-container'>
+        <div className='footer-column'>
+          <h3>Dibby</h3>
+          <p>
+            Your number one site for making the renting process as easy as
+            possible.
+          </p>
+          <h4>Follow Us</h4>
+          <div className='social-icons'>
+            <a href='https://twitter.com' target='_blank' rel='noopener noreferrer'>
+              <FaTwitter />
+            </a>
+            <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+              <FaInstagram />
+            </a>
+            <a href='https://linkedin.com' target='_blank' rel='noopener noreferrer'>
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+
+        <div className='footer-column'>
+          <h4>Become a Viewer</h4>
+          <ul>
+            <li><a href='/create-profile'>Create a profile</a></li>
+            <li><a href='/viewers-hub'>Viewer’s hub</a></li>
+            <li><a href='/inbox'>Inbox</a></li>
+          </ul>
+        </div>
+
+        <div className='footer-column'>
+          <h4>Post Your Own Listing</h4>
+          <ul>
+            <li><a href='/post-listing'>Post a listing</a></li>
+            <li><a href='/your-listings'>Your listings</a></li>
+            <li><a href='/boost-listing'>Boost your listing</a></li>
+          </ul>
+        </div>
+
+        <div className='footer-column'>
+          <h4>Help</h4>
+          <ul>
+            <li><a href='/faq'>FAQ</a></li>
+            <li><a href='/about-us'>About us</a></li>
+            <li><a href='/testimonials'>Testimonials</a></li>
+            <li><a href='/contact'>Contact us</a></li>
+          </ul>
+        </div>
+
+        <div className='footer-column policy-links'>
+          <ul>
+            <li><a href='/terms'>Terms and Conditions</a></li>
+            <li><a href='/privacy'>Privacy Policy</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
