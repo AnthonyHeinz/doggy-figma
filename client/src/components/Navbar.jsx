ebf6ad1fb708c762ca36dd4abadfde653106a45a
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-06-26 14:48:25 -0400

    Added a placerholder handle click

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,15 +3,28 @@ import './navbar.css';
 import dibbyLogo from '../assets/dibby_Logo.svg';
 
 function Navbar() {
+  const handleAddPropertyButton = () => {
+    console.log('This will eventually add a property');
+  };
+
   return (
     <header className='navbar'>
       <div className='navbar-left'>
         <img src={dibbyLogo} alt='Dibby logo' className='navbar-logo' />
       </div>
       <nav className='navbar-right'>
-        <a href='#' className='nav-link'>About</a>
-        <a href='#' className='nav-link'>Sign In / Sign Up</a>
-        <button className='add-property-btn'>Add Property</button>
+        <a href='#' className='nav-link'>
+          About
+        </a>
+        <a href='#' className='nav-link'>
+          Sign In / Sign Up
+        </a>
+        <button
+          className='add-property-btn'
+          onClick={() => handleAddPropertyButton()}
+        >
+          Add Property
+        </button>
       </nav>
     </header>
   );
