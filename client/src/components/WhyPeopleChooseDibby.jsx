648ef2b920d1def223cb8cbbc7817463d9a4264b
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-06-27 14:35:50 -0400

    Created first iteration of the carasel

--- client/src/components/WhyPeopleChooseDibby.jsx
+++ client/src/components/WhyPeopleChooseDibby.jsx
@@ -1,27 +1,72 @@
 import React from 'react';
+import { useState } from 'react';
 import './whyPeopleChooseDibby.css';
 import dogIcon from '../assets/dibby_Dog_Logo.png';
-
-const testimonials = [
-  {
-    name: 'NAME',
-    quote: `This honestly made life so much easier. I honestly was pretty nervous about moving with my sister, but Dibby gave us a lot of comfort in our decision.`,
-  },
-  {
-    name: 'NAME',
-    quote: `This was crazy helpful. So many apartment tours are during the week and it’s hard to take enough time off to make it worth the cost of the work day.`,
-  },
-  {
-    name: 'NAME',
-    quote: `There is no way me and my partner would have been able to move as quickly without Dibby’s help.`,
-  },
-  {
-    name: 'NAME',
-    quote: `Something was going on with the property manager, and we received very helpful advice.`,
-  },
-];
+import dogImage from '../assets/testdog.jpeg';
 
 function WhyPeopleChooseDibby() {
+  const testimonials = [
+    {
+      name: 'Doggo',
+      quote: `This honestly made life so much easier. I honestly was pretty nervous about moving with my sister, but Dibby gave us a lot of comfort in our decision.`,
+      image: dogImage 
+    },
+    {
+      name: 'NAME',
+      quote: `This was crazy helpful. So many apartment tours are during the week and it’s hard to take enough time off to make it worth the cost of the work day.`,
+      image: dogImage 
+    },
+    {
+      name: 'NAME',
+      quote: `There is no way me and my partner would have been able to move as quickly without Dibby’s help.`,
+      image:  dogImage ,
+    },
+    {
+      name: 'NAME',
+      quote: `Something was going on with the property manager, and we received very helpful advice.`,
+      image:  dogImage ,
+    },
+    {
+      name: 'NAME',
+      quote: `This honestly made life so much easier. I honestly was pretty nervous about moving with my sister, but Dibby gave us a lot of comfort in our decision.`,
+      image:  dogImage ,
+    },
+    {
+      name: 'NAME',
+      quote: `This was crazy helpful. So many apartment tours are during the week and it’s hard to take enough time off to make it worth the cost of the work day.`,
+      image:  dogImage ,
+    },
+    {
+      name: 'NAME',
+      quote: `There is no way me and my partner would have been able to move as quickly without Dibby’s help.`,
+      image:  dogImage ,
+    },
+    {
+      name: 'NAME',
+      quote: `Something was going on with the property manager, and we received very helpful advice.`,
+      image:  dogImage ,
+    },
+  ];
+
+  const [startIndex, setStartIndex] = useState(0);
+
+  const visableCount = 4;
+
+  const handlePrev = () => {
+    setStartIndex((prev) => Math.max(prev - 1, 0));
+  };
+
+  const handleNext = () => {
+    setStartIndex((prev) =>
+      Math.min(prev + 1, testimonials.length - visableCount)
+    );
+  };
+
+  const visibleCards = testimonials.slice(
+    startIndex,
+    startIndex + visableCount
+  );
+
   return (
     <section className='testimonials-section'>
       <div className='how-it-works-gradient-three'></div>
@@ -29,16 +74,36 @@ function WhyPeopleChooseDibby() {
       <p className='subtitle'>
         Hear what real customers have to say about their Dibby experience.
       </p>
-      <div className='testimonials-grid'>
-        {testimonials.map((item, idx) => (
-          <div className='testimonial-card' key={idx}>
-            <div className='testimonial-header'>
-              <div className='avatar' />
-              <p className='name'>{item.name}</p>
+      <div className='carousel-wrapper'>
+        <button
+          className='carousel-btn'
+          onClick={handlePrev}
+          disabled={startIndex === 0}
+        >
+          ‹
+        </button>
+        <div className='carousel-track'>
+          {visibleCards.map((card, index) => (
+            <div className='carousel-card testimonial-card' key={index}>
+              <div
+                className='testimonial-background'
+                style={{ backgroundImage: `url(${card.image})` }}
+              >
+                <div className='testimonial-overlay'>
+                  <p className='quote'>“{card.quote}”</p>
+                  <p className='name'>– {card.name}</p>
+                </div>
+              </div>
             </div>
-            <p className='quote'>“{item.quote}”</p>
-          </div>
-        ))}
+          ))}
+        </div>
+        <button
+          className='carousel-btn'
+          onClick={handleNext}
+          disabled={startIndex + visableCount >= testimonials.length}
+        >
+          ›
+        </button>
       </div>
       <img src={dogIcon} alt='dog' className='dog-icon-bottom-left' />
       <img src={dogIcon} alt='dog' className='dog-icon-top-right' />
@@ -49,3 +114,4 @@ function WhyPeopleChooseDibby() {
 }
 
 export default WhyPeopleChooseDibby;
+
