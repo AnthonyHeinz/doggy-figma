6680cb4914d31a380730beb4c76c69e11ff47389
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-01 14:24:01 -0400

    Refactored code to have ListingBar as a component

---
+++ client/src/components/PropertyListings/ListingBar/index.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import PropertySearch from '../../../assets/Property_Listing_Search.png';
+import dogIcon from '../../../assets/dibby_Dog_Logo.png';
+import PriceDropDown from '../PriceDropdown/index.jsx';
+import BedAndBathDropDown from '../BedAndBathDropdown/index.jsx';
+import HomeTypeDropDown from '../HomeTypeDropDown/index.jsx';
+import LeaseTypeDropDown from '../LeaseTypeDropDown/index.jsx';
+import './styles.css'
+
+function ListingBar({
+  searchValue,
+  onSearchChange,
+  onToggleDropdown,
+  dropdownStates,
+}) {
+  return (
+    <section id='property-listing-bar'>
+      <div id='property-listing-inputs'>
+        <div id='property-listing-search-bar-wrapper'>
+          <input
+            type='text'
+            id='property-listing-search-bar'
+            placeholder='Enter city or ZIP Code'
+            value={searchValue}
+            onChange={(e) => onSearchChange(e.target.value)}
+          />
+          <button id='property-listing-search-button'>
+            <img
+              src={PropertySearch}
+              alt='Property Search'
+              id='property-listing-search-glass'
+            ></img>
+          </button>
+        </div>
+        <button
+          type='text'
+          id='property-listing-price'
+          className={dropdownStates.price ? 'selected' : ''}
+          onClick={() => onToggleDropdown('price')}
+        >
+          Price
+          <span
+            className={`property-listing-custom-arrow ${
+              dropdownStates.price ? 'selected' : ''
+            }`}
+          />
+        </button>
+        {dropdownStates.price && (
+          <PriceDropDown onClose={() => onToggleDropdown('price')} />
+        )}
+
+        <button
+          type='text'
+          id='property-listing-bed-bath'
+          className={dropdownStates.bedsBaths ? 'selected' : ''}
+          onClick={() => onToggleDropdown('bedsBaths')}
+        >
+          Beds & Bath
+          <span
+            className={`property-listing-custom-arrow ${
+              dropdownStates.bedsBaths ? 'selected' : ''
+            }`}
+          />
+        </button>
+        {dropdownStates.bedsBaths && (
+          <BedAndBathDropDown onClose={() => onToggleDropdown('bedsBaths')} />
+        )}
+        <button
+          type='text'
+          className={`property-listing-home-lease-type ${
+            dropdownStates.homeType ? 'selected' : ''
+          }`}
+          onClick={() => onToggleDropdown('homeType')}
+        >
+          Home Type
+          <span
+            className={`property-listing-custom-arrow ${
+              dropdownStates.homeType ? 'selected' : ''
+            }`}
+          />
+        </button>
+        {dropdownStates.homeType && (
+          <HomeTypeDropDown onClose={() => onToggleDropdown('homeType')} />
+        )}
+        <button
+          type='text'
+          className={`property-listing-home-lease-type ${
+            dropdownStates.leaseType ? 'selected' : ''
+          }`}
+          onClick={() => onToggleDropdown('leaseType')}
+        >
+          Lease Type
+          <span
+            className={`property-listing-custom-arrow ${
+              dropdownStates.leaseType ? 'selected' : ''
+            }`}
+          />
+        </button>
+        {dropdownStates.leaseType && (
+          <LeaseTypeDropDown onClose={() => onToggleDropdown('leaseType')} />
+        )}
+      </div>
+      <img src={dogIcon} alt='dogIcon' id='property-listing-dog-icon'></img>
+    </section>
+  );
+}
+
+export default ListingBar
--- client/src/pages/PropertyListings.jsx
+++ client/src/pages/PropertyListings.jsx
@@ -1,115 +1,34 @@
 import React from 'react';
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
-import dogIcon from '../assets/dibby_Dog_Logo.png';
 import './propertyListings.css';
-import PropertySearch from '../assets/Property_Listing_Search.png';
-import PriceDropDown from '../components/PropertyListings/PriceDropdown/index.jsx';
-import BedAndBathDropDown from '../components/PropertyListings/BedAndBathDropdown/index.jsx';
-import HomeTypeDropDown from '../components/PropertyListings/HomeTypeDropDown/index.jsx';
-import LeaseTypeDropDown from '../components/PropertyListings/LeaseTypeDropDown/index.jsx';
 import PropertyListingContainer from '../components/PropertyListings/PropertyListingContainer/index.jsx';
+import ListingBar from '../components/PropertyListings/ListingBar/index.jsx';
 
 function PropertyListings() {
-  const [priceDropDown, setPriceDropDown] = useState(false);
-  const [bedAndBathDropDown, setBedAndBathDropDown] = useState(false);
-  const [homeTypeDropDown, setHomeTypeDropDown] = useState(false);
-  const [leaseTypeDropDown, setLeaseTypeDropDown] = useState(false);
-  const togglePriceDropdown = () => setPriceDropDown(!priceDropDown);
-  const toggleBedAndBathDropdown = () =>
-    setBedAndBathDropDown(!bedAndBathDropDown);
-  const toggleHomeTypeDropDown = () => setHomeTypeDropDown(!homeTypeDropDown);
-  const toggleLeaseTypeDropDown = () =>
-    setLeaseTypeDropDown(!leaseTypeDropDown);
+  const [searchValue, setSearchValue] = useState('');
+  const [dropdownStates, setDropdownStates] = useState({
+    price: false,
+    bedsBaths: false,
+    homeType: false,
+    leaseType: false,
+  });
 
+  const toggleDropdown = (key) => {
+    setDropdownStates((prev) => ({
+      ...prev,
+      [key]: !prev[key],
+    }));
+  };
   return (
     <div>
       <Navbar />
-      <section id='property-listing-bar'>
-        <div id='property-listing-inputs'>
-          <div className='property-listing-search-bar-wrapper'>
-            <input
-              type='text'
-              id='property-listing-search-bar'
-              placeholder='Enter city or ZIP Code'
-            />
-            <button id='property-listing-search-button'>
-              <img
-                src={PropertySearch}
-                alt='Property Search'
-                id='property-listing-search-glass'
-              ></img>
-            </button>
-          </div>
-          <button
-            type='text'
-            id='property-listing-price'
-            className={priceDropDown ? 'selected' : ''}
-            onClick={togglePriceDropdown}
-          >
-            Price
-            <span
-              className={`property-listing-custom-arrow ${
-                priceDropDown ? 'selected' : ''
-              }`}
-            />
-          </button>
-          {priceDropDown && (
-            <PriceDropDown onClose={() => setPriceDropDown(false)} />
-          )}
-          <button
-            type='text'
-            id='property-listing-bed-bath'
-            className={bedAndBathDropDown ? 'selected' : ''}
-            onClick={toggleBedAndBathDropdown}
-          >
-            Beds & Bath
-            <span
-              className={`property-listing-custom-arrow ${
-                bedAndBathDropDown ? 'selected' : ''
-              }`}
-            />
-          </button>
-          {bedAndBathDropDown && (
-            <BedAndBathDropDown onClose={() => setBedAndBathDropDown(false)} />
-          )}
-          <button
-            type='text'
-            className={`property-listing-home-lease-type ${
-              homeTypeDropDown ? 'selected' : ''
-            }`}
-            onClick={toggleHomeTypeDropDown}
-          >
-            Home Type
-            <span
-              className={`property-listing-custom-arrow ${
-                homeTypeDropDown ? 'selected' : ''
-              }`}
-            />
-          </button>
-          {homeTypeDropDown && (
-            <HomeTypeDropDown onClose={() => setHomeTypeDropDown(false)} />
-          )}
-          <button
-            type='text'
-            className={`property-listing-home-lease-type ${
-              leaseTypeDropDown ? 'selected' : ''
-            }`}
-            onClick={toggleLeaseTypeDropDown}
-          >
-            Lease Type
-            <span
-              className={`property-listing-custom-arrow ${
-                leaseTypeDropDown ? 'selected' : ''
-              }`}
-            />
-          </button>
-          {leaseTypeDropDown && (
-            <LeaseTypeDropDown onClose={() => setLeaseTypeDropDown(false)} />
-          )}
-        </div>
-        <img src={dogIcon} alt='dogIcon' id='property-listing-dog-icon'></img>
-      </section>
+      <ListingBar
+        searchValue={searchValue}
+        onSearchChange={setSearchValue}
+        dropdownStates={dropdownStates}
+        onToggleDropdown={toggleDropdown}
+      />
       <div className='property-listing-results-map-container'>
         <section className='property-listing-map'></section>
         <PropertyListingContainer location='Los Angeles, CA' />
