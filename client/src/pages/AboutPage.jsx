b58698e06b2df0a9c8575fed20475037dfa897c2
Brandon <bbeuttel@gmail.com>
2025-07-02 10:14:03 -0700

    added in about page route, about page, nav bar and footer to about page

---
+++ client/src/pages/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/Navbar/index.jsx'
+import Footer from '../components/LandingPageSections/Footer/index.jsx'
+
+function AboutPage () {
+    return(
+        <div>
+            <NavBar />
+            <Footer />
+        </div>
+    )
+}
+
+export default AboutPage;
