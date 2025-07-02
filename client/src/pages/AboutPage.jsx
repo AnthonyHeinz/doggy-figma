61d4d5646d53536d4716d54548f6f4f17324dc89
Brandon <bbeuttel@gmail.com>
2025-07-02 15:19:42 -0700

    updated about page components, hero component jsx and css complete aside from the shading part.

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import NavBar from '../components/Navbar/index.jsx'
-import Footer from '../components/LandingPageSections/Footer/index.jsx'
+import NavBar from '../components/Navbar/index.jsx';
+import Footer from '../components/LandingPageSections/Footer/index.jsx';
+import HeroSection from '../components/AboutPageSections/HeroSection/index.jsx';
 
 function AboutPage () {
     return(
         <div>
             <NavBar />
+            <HeroSection />
             <Footer />
         </div>
     )
