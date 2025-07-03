fab306e8bfd95f3cb63da625de37dff77737fd4f
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 17:58:51 -0400

    First iteration of photos

--- client/src/components/PropertyDetails/PropertyPhotos/index.jsx
+++ client/src/components/PropertyDetails/PropertyPhotos/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 import './styles.css';
+import testDog from '../../../assets/testdog.jpeg';
 
 function PropertyPhotos() {
+  const [images, setImages] = useState([
+    testDog,
+    testDog,
+    testDog,
+    testDog,
+    testDog,
+  ]);
   return (
-    <div>
-      <h1>Test</h1>
+    <div className='property-details-photo-container'>
+      <div className='property-details-main-photo-container'>
+        <img
+          src={images[0]}
+          alt='Main Photo'
+          className='property-details-main-image'
+        />
+      </div>
+      <div className='property-details-photo-grid'>
+        {images.slice(1).map((src, index) => (
+          <img
+            key={index}
+            src={src}
+            alt={`Gallery ${index + 1}`}
+            className='property-details-image '
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- client/src/pages/PropertyDetails.jsx
+++ client/src/pages/PropertyDetails.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import NavBar from '../components/Navbar/index.jsx';
 import Footer from '../components/Footer/index.jsx';
 import ListingContainer from '../components/PropertyDetails/ListingContainer/index.jsx';
+import PropertyPhotos from '../components/PropertyDetails/PropertyPhotos/index.jsx';
 
 function PropertyDetails() {
   return (
     <div>
       <NavBar />
+      <PropertyPhotos />
       <ListingContainer />
       <Footer />
     </div>
