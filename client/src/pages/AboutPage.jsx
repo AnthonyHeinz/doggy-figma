a488e7a27f86305b07ae7fe39448520250447a9f
Brandon <bbeuttel@gmail.com>
2025-07-02 20:12:20 -0700

    completed who are we component and related CSS.  still need to add in accents

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from '../components/Navbar/index.jsx';
 import Footer from '../components/LandingPageSections/Footer/index.jsx';
 import HeroSection from '../components/AboutPageSections/HeroSection/index.jsx';
+import WhoAreWe from '../components/AboutPageSections/WhoAreWeSection/index.jsx'
 
 function AboutPage () {
     return(
         <div>
             <NavBar />
             <HeroSection />
+            <WhoAreWe />
             <Footer />
         </div>
     )
