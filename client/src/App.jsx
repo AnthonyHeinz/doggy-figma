d84de2b4ad00a56ab85f09a41f281990a615ea2e
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 12:16:54 -0400

    Added footer and navbar to property details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import PropertyListings from './pages/PropertyListings.jsx';
+import PropertyDetails from './pages/PropertyDetails.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/property-listings' element={<PropertyListings />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/property-listings' element={<PropertyListings />} />
+        <Route path='/property-details' element={<PropertyDetails />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/PropertyDetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/Navbar/index.jsx';
+import Footer from '../components/Footer/index.jsx';
+
+function PropertyDetails() {
+  return (
+    <div>
+      <NavBar />
+      <Footer />
+    </div>
+  );
+}
+
+export default PropertyDetails;
