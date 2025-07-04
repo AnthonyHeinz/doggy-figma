fecbbf1eac8443190e9a2b0b6c38c3a272ce1da0
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 21:38:28 -0400

    Created both sides of popup, need to fix spacing

--- client/src/components/ConfirmAndPayPopUp/index.jsx
+++ client/src/components/ConfirmAndPayPopUp/index.jsx
@@ -18,7 +18,7 @@ function ConfirmAndPayPopUp({
         className='confirm-pay-popup-box'
         onClick={(e) => e.stopPropagation()}
       >
-        <div id='confirm-pay-left-side'>
+        <section id='confirm-pay-left-side'>
           <h5>Confirm and Pay</h5>
           <div id='confirm-pay-location-info'>
             <h6>Send a Viewer to this Address:</h6>
@@ -35,7 +35,43 @@ function ConfirmAndPayPopUp({
               </p>
             </div>
           </div>
-        </div>
+          <div id='confirm-pay-viewing-details'>
+            <p>Viewings will be completed within 3 business days.</p>
+            <p>Viewing report will include the following:</p>
+            <ul className='confirm-pay-list'>
+              <li>Interior and exterior photos/videos</li>
+              <li>Written report</li>
+              <li>FaceTime call</li>
+              <li>Neighborhood tour</li>
+              <li>Street parking report</li>
+              <li>Smell and noise level tests</li>
+            </ul>
+          </div>
+        </section>
+        <section id='confirm-pay-right-side'>
+          <div className='confirm-pay-step-container'>
+            <h6>
+              1. Payment Price: <span className='confirm-pay-price'>$49</span>
+            </h6>
+
+            <h6>2. Enter contact information</h6>
+            <input type='text' placeholder='Full Name' />
+            <input type='text' placeholder='Phone Number' />
+            <input type='email' placeholder='Email Address' />
+
+            <h6>3. Add Payment Method</h6>
+            <input type='text' placeholder='Card Number' />
+            <div className='confirm-pay-input-row'>
+              <input type='text' placeholder='MM/YY' />
+              <input type='text' placeholder='CVC' />
+            </div>
+            <input type='text' placeholder='ZIP Code' />
+            <input type='text' placeholder='Country/Region' />
+            <button id='confirm-pay-send-a-viewer-button'>
+              Confirm and Pay
+            </button>
+          </div>
+        </section>
       </div>
     </div>
   );
