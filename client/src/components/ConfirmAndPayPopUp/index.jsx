0b539cc928edce5f411961ac02b81b01387523ff
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 21:05:52 -0400

    Updated location on popup

--- client/src/components/ConfirmAndPayPopUp/index.jsx
+++ client/src/components/ConfirmAndPayPopUp/index.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './styles.css';
 
-function ConfirmAndPayPopUp() {
+function ConfirmAndPayPopUp({
+  isOpen,
+  onClose,
+  backgroundImage,
+  beds,
+  baths,
+  sqft,
+  location,
+  buildingName,
+}) {
+  if (!isOpen) return null;
   return (
-    <div id='confirm-pay-popup-container'>
-      <div id='confirm-pay-info-container'>
-        <div id='confirm-pay-location-info'>
+    <div className='confirm-pay-popup-overlay' onClick={onClose}>
+      <div
+        className='confirm-pay-popup-box'
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div id='confirm-pay-left-side'>
           <h5>Confirm and Pay</h5>
+          <div id='confirm-pay-location-info'>
+            <h6>Send a Viewer to this Address:</h6>
+            <img
+              src={backgroundImage}
+              alt='location-picture'
+              id='confirm-pay-picture'
+            />
+            <div className='confirm-pay-info'>
+              <p className='confirm-pay-address'>{location}</p>
+              <p className='confirm-pay-building'>{buildingName}</p>
+              <p className='confirm-pay-details'>
+                {beds} bds | {baths} ba | {sqft} sqft
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
