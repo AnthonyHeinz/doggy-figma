d734a0a70d7f113544c3d0df7460d3b654421e7a
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 20:58:40 -0400

    Updated Tour property state

--- client/src/components/PropertyDetails/TourThisProperty/index.jsx
+++ client/src/components/PropertyDetails/TourThisProperty/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 import './styles.css';
 import dogIcon from '../../../assets/dibby_Dog_Logo.png';
+import ConfirmAndPayPopUp from '../../ConfirmAndPayPopUp';
+import testDog from '../../../assets/testdog.jpeg';
 
 function TourThisProperty() {
+  const [showSendViewerPopup, setShowSendViewerPopup] = useState(false);
+  const [houseDetails, setHouseDetails] = useState({
+    location: '627 Belmont Ave #6, Los Angeles, CA 90026',
+    buildingName: 'Belmont Apartments',
+    beds: 2,
+    baths: 1,
+    sqft: 875,
+  });
+
   return (
     <div id='property-details-tour-this-property-container'>
       <img src={dogIcon} alt='dog icon' className='property-details-dog-icon' />
@@ -21,7 +33,22 @@ function TourThisProperty() {
           <li>Smell and noise level tests</li>
         </ul>
       </div>
-      <button id='property-details-send-a-viewer-button'>Send a Viewer</button>
+      <button
+        id='property-details-send-a-viewer-button'
+        onClick={() => setShowSendViewerPopup(true)}
+      >
+        Send a Viewer
+      </button>
+      <ConfirmAndPayPopUp
+        isOpen={showSendViewerPopup}
+        onClose={() => setShowSendViewerPopup(false)}
+        backgroundImage={testDog}
+        beds={houseDetails.beds}
+        baths={houseDetails.baths}
+        sqft={houseDetails.sqft}
+        location={houseDetails.location}
+        buildingName={houseDetails.buildingName}
+      />
     </div>
   );
 }
