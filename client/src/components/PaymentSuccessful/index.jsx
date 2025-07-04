b505c6e439953ed5258624399858606e0e7691ac
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-04 16:37:09 -0400

    Created Payment Successful Popup

--- client/src/components/PaymentSuccessful/index.jsx
+++ client/src/components/PaymentSuccessful/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import greenCheck from '../../assets/green_Check_Mark.png';
 
 function PaymentSuccessful({ isOpen, onClose }) {
   if (!isOpen) return null;
@@ -12,6 +13,22 @@ function PaymentSuccessful({ isOpen, onClose }) {
         <button className='payment-successful-close-button' onClick={onClose}>
           ×
         </button>
+
+        <div id='payment-successful-word-box'>
+          <img
+            src={greenCheck}
+            alt='Green-Check'
+            id='payment-successful-green-check'
+          />
+          <h5>Payment Successful</h5>
+          <p>
+            A confirmation email has been sent to you. Your Viewer will send
+            your report within 3 business days.
+          </p>
+          <button id='payment-successful-send-a-viewer-button'>
+            Return to Property Listing
+          </button>
+        </div>
       </div>
     </div>
   );
