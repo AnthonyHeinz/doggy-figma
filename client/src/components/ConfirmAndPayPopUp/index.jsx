c372517acf5db65c340cbd9ead4f7e33df41fcb2
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 20:24:39 -0400

    Working on Confirm Pay popup

--- client/src/components/ConfirmAndPayPopUp/index.jsx
+++ client/src/components/ConfirmAndPayPopUp/index.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import './styles.css'
+import React from 'react';
+import './styles.css';
 
-function ConfirmAndPayPopUp(){
-<div id = 'confirm-pay-popup-container'>
-
-</div>
+function ConfirmAndPayPopUp() {
+  return (
+    <div id='confirm-pay-popup-container'>
+      <div id='confirm-pay-info-container'>
+        <div id='confirm-pay-location-info'>
+          <h5>Confirm and Pay</h5>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ConfirmAndPayPopUp
+export default ConfirmAndPayPopUp;
