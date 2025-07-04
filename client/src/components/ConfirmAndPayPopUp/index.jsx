1081fe57f43f9d53a3ddd6e4f299c0f9fdad974e
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-03 22:21:09 -0400

    Updated confirm pay popup

--- client/src/components/ConfirmAndPayPopUp/index.jsx
+++ client/src/components/ConfirmAndPayPopUp/index.jsx
@@ -18,61 +18,74 @@ function ConfirmAndPayPopUp({
         className='confirm-pay-popup-box'
         onClick={(e) => e.stopPropagation()}
       >
-        <section id='confirm-pay-left-side'>
-          <h5>Confirm and Pay</h5>
-          <div id='confirm-pay-location-info'>
-            <h6>Send a Viewer to this Address:</h6>
-            <img
-              src={backgroundImage}
-              alt='location-picture'
-              id='confirm-pay-picture'
-            />
-            <div className='confirm-pay-info'>
-              <p className='confirm-pay-address'>{location}</p>
-              <p className='confirm-pay-building'>{buildingName}</p>
-              <p className='confirm-pay-details'>
-                {beds} bds | {baths} ba | {sqft} sqft
-              </p>
+        <div className='confirm-pay-popup-header'>
+          <button
+            className='confirm-pay-back-button'
+            onClick={() => console.log('Back')}
+          >
+            {'<'} Back
+          </button>
+          <button className='confirm-pay-close-button' onClick={onClose}>
+            ×
+          </button>
+        </div>
+        <div id='confirm-pay-body'>
+          <section id='confirm-pay-left-side'>
+            <h5>Confirm and Pay</h5>
+            <div id='confirm-pay-location-info'>
+              <h6>Send a Viewer to this Address:</h6>
+              <img
+                src={backgroundImage}
+                alt='location-picture'
+                id='confirm-pay-picture'
+              />
+              <div className='confirm-pay-info'>
+                <p className='confirm-pay-address'>{location}</p>
+                <p className='confirm-pay-building'>{buildingName}</p>
+                <p className='confirm-pay-details'>
+                  {beds} bds | {baths} ba | {sqft} sqft
+                </p>
+              </div>
             </div>
-          </div>
-          <div id='confirm-pay-viewing-details'>
-            <h6>Viewing Details</h6>
-            <p>Viewings will be completed within 3 business days.</p>
-            <p>Viewing report will include the following:</p>
-            <ul className='confirm-pay-list'>
-              <li>Interior and exterior photos/videos</li>
-              <li>Written report</li>
-              <li>FaceTime call</li>
-              <li>Neighborhood tour</li>
-              <li>Street parking report</li>
-              <li>Smell and noise level tests</li>
-            </ul>
-          </div>
-        </section>
-        <section id='confirm-pay-right-side'>
-          <div className='confirm-pay-step-container'>
-            <h6>
-              1. Payment Price: <span className='confirm-pay-price'>$49</span>
-            </h6>
-
-            <h6>2. Enter contact information</h6>
-            <input type='text' placeholder='Full Name' />
-            <input type='text' placeholder='Phone Number' />
-            <input type='email' placeholder='Email Address' />
-
-            <h6>3. Add Payment Method</h6>
-            <input type='text' placeholder='Card Number' />
-            <div className='confirm-pay-input-row'>
-              <input type='text' placeholder='MM/YY' />
-              <input type='text' placeholder='CVC' />
+            <div id='confirm-pay-viewing-details'>
+              <h6>Viewing Details</h6>
+              <p>Viewings will be completed within 3 business days.</p>
+              <p>Viewing report will include the following:</p>
+              <ul className='confirm-pay-list'>
+                <li>Interior and exterior photos/videos</li>
+                <li>Written report</li>
+                <li>FaceTime call</li>
+                <li>Neighborhood tour</li>
+                <li>Street parking report</li>
+                <li>Smell and noise level tests</li>
+              </ul>
             </div>
-            <input type='text' placeholder='ZIP Code' />
-            <input type='text' placeholder='Country/Region' />
+          </section>
+          <section id='confirm-pay-right-side'>
+            <div className='confirm-pay-step-container'>
+              <h6>
+                1. Payment Price: <span className='confirm-pay-price'>$49</span>
+              </h6>
+
+              <h6>2. Enter contact information</h6>
+              <input type='text' placeholder='Full Name' />
+              <input type='text' placeholder='Phone Number' />
+              <input type='email' placeholder='Email Address' />
+
+              <h6>3. Add Payment Method</h6>
+              <input type='text' placeholder='Card Number' />
+              <div className='confirm-pay-input-row'>
+                <input type='text' placeholder='MM/YY' />
+                <input type='text' placeholder='CVC' />
+              </div>
+              <input type='text' placeholder='ZIP Code' />
+              <input type='text' placeholder='Country/Region' />
+            </div>
             <button id='confirm-pay-send-a-viewer-button'>
               Confirm and Pay
             </button>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     </div>
   );
