a8acfa70413fbfd51b647aae76b1da4ff356aceb
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-04 16:55:34 -0400

    Added back to search button to property details

--- client/src/components/PropertyDetails/PropertyPhotos/index.jsx
+++ client/src/components/PropertyDetails/PropertyPhotos/index.jsx
@@ -12,23 +12,28 @@ function PropertyPhotos() {
     testDog,
   ]);
   return (
-    <div className='property-details-photo-container'>
-      <div className='property-details-main-photo-container'>
-        <img
-          src={images[0]}
-          alt='Main Photo'
-          className='property-details-main-image'
-        />
+    <div id = 'property-details-back-search-photos-container'>
+      <div className='property-details-back-to-search'>
+        <div> {'<'} Back to Search</div>
       </div>
-      <div className='property-details-photo-grid'>
-        {images.slice(1).map((src, index) => (
+      <div className='property-details-photo-container'>
+        <div className='property-details-main-photo-container'>
           <img
-            key={index}
-            src={src}
-            alt={`Gallery ${index + 1}`}
-            className='property-details-image '
+            src={images[0]}
+            alt='Main Photo'
+            className='property-details-main-image'
           />
-        ))}
+        </div>
+        <div className='property-details-photo-grid'>
+          {images.slice(1).map((src, index) => (
+            <img
+              key={index}
+              src={src}
+              alt={`Gallery ${index + 1}`}
+              className='property-details-image '
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
