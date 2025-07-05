1d7ad470db595cc1b8d04b95d524cb3e90babed1
Brandon <bbeuttel@gmail.com>
2025-07-05 13:28:34 -0700

    proplist card complete added in second compoent for prop card

--- client/src/components/ProfilePage/PropertyListingCard/index.jsx
+++ client/src/components/ProfilePage/PropertyListingCard/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import Pencil from '../../../assets/updatePencil.png';
 import PreviewEye from '../../../assets/previewicons.png';
 import TrashCan from '../../../assets/TrashCan.png';
@@ -7,42 +7,53 @@ import './styles.css';
 
 function PropertyListingCard({
   hardCodedListingInfo,
-  published,
-  setPublished,
 }) {
+    const [published, setPublished] = useState('true');
+    
   return (
     <div className='card-wrapper'>
       <div className='card-content'>
-        <img src={PropertyPic} alt='property-pic' />
-        <div className='listing-info'>
-          <button
-            onClick={() =>
-              setPublished(published === 'true' ? 'false' : 'true')
-            }
-            className={published === 'true' ? 'published' : 'unpublished'}
-          >
-            {published === 'true' ? 'Published' : 'Unpublished'}
-          </button>
-          <div className='address'>{hardCodedListingInfo.address}</div>
-          <div className='unitInfo'>
-            {hardCodedListingInfo.beds} bds | {hardCodedListingInfo.bath} ba |{' '}
-            {hardCodedListingInfo.sqft} sqft
+        <img src={PropertyPic} alt='property-pic' className='property-pic' />
+        <div className='card-content-text'>
+          <div className='listing-info'>
+            <div className='listing-info-top-container'>
+              <div className='apartment-details'>
+              <div className='address'>{hardCodedListingInfo.address}</div>
+              <div className='unit-info'>
+                {hardCodedListingInfo.beds} bds | {hardCodedListingInfo.bath} ba
+                | {hardCodedListingInfo.sqft} sqft
+              </div>
+              <div className='listing-price'>
+                Listing Price: ${hardCodedListingInfo.price}/mo
+              </div>
+              </div>
+            <button
+              onClick={() =>
+                setPublished(published === 'true' ? 'false' : 'true')
+              }
+              className={published === 'true' ? 'published' : 'unpublished'}
+            >
+              {published === 'true' ? 'Published' : 'Unpublished'}
+            </button>
+            </div>
+          <div className='listing-info-bottom-container'>
+            <div className='date-added'>
+              Added: {hardCodedListingInfo.added}
+            </div>
+            <div className='listing-info-buttons'>
+              <button className='edit'>
+                <img src={Pencil} alt='pencil' className='edit-pencil' /> <span>Edit</span>
+              </button>
+              <button className='delete'>
+                <img src={TrashCan} alt='trash' className='trash-can' /> <span>Delete</span>
+              </button>
+              <button className='preview'>
+                <img src={PreviewEye} alt='eye' className='preview-eye' />{' '}
+                <span>Preview</span>
+              </button>
+            </div>
+            </div>
           </div>
-          <div className='listing-price'>
-            Listing Price: ${hardCodedListingInfo.price}/mo
-          </div>
-        </div>
-        <div className='date-added'>Added: {hardCodedListingInfo.added}</div>
-        <div className='listing-info-buttons'>
-          <button className='edit'>
-            <img src={Pencil} alt='pencil' className='edit-pencikl' /> Edit
-          </button>
-          <button className='delete'>
-            <img src={TrashCan} alt='trash' className='trash-can' /> Delete
-          </button>
-          <button className='preview'>
-            <img src={PreviewEye} alt='eye' className='preview-eye' /> Preview
-          </button>
         </div>
       </div>
     </div>
--- client/src/components/ProfilePage/ProperyListings/index.jsx
+++ client/src/components/ProfilePage/ProperyListings/index.jsx
@@ -1,10 +1,9 @@
-import { React, useState } from 'react';
+import React from 'react';
 import DogIcon from '../../../assets/dibby_Dog_Logo.png';
 import PropertyListingCard from '../PropertyListingCard/index.jsx';
 import './styles.css';
 
 function MyPropertyListings() {
-  const [published, setPublished] = useState('true');
 
   const hardCodedListingInfo = {
     address: '627 Belmont Ave #6, Los Angeles, CA 90026',
@@ -20,7 +19,8 @@ function MyPropertyListings() {
       <div className='my-prop-list'>
         <h4>My Property Listings</h4>
         <div className='listings'>
-            <PropertyListingCard published={published} setPublished={setPublished} hardCodedListingInfo={hardCodedListingInfo} />
+            <PropertyListingCard hardCodedListingInfo={hardCodedListingInfo} />
+             <PropertyListingCard hardCodedListingInfo={hardCodedListingInfo} />
         </div>
       </div>
       <img src={DogIcon} alt='dogIcon' className='my-prop-list-dog-icon' />
