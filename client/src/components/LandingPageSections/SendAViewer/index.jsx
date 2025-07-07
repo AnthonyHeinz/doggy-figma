d05304fba9fbee04befa8df420ab131755cf72ce
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-07 11:34:36 -0400

    Working on popup

--- client/src/components/LandingPageSections/SendAViewer/index.jsx
+++ client/src/components/LandingPageSections/SendAViewer/index.jsx
@@ -1,12 +1,61 @@
 import React from 'react';
 import './styles.css';
+import { useState } from 'react';
 
 function SendAViewer({ isOpen, onClose }) {
+  const [showConfirmAndPayPopup, setShowConfirmAndPayPopup] = useState(false);
   if (!isOpen) return null;
 
-  return(
-    <div>Test</div>
-  )
+  return (
+    <div className='landing-send-a-viewer-popup-overlay'>
+      <div className='landing-send-a-viewer-popup-box'>
+        <button
+          className='landing-send-a-viewer-close-button'
+          onClick={onClose}
+        >
+          ×
+        </button>
+        <div className='landing-send-a-viewer-words-input-box'>
+          <h5>Send a Viewer</h5>
+          <p>
+            For $49, you can send one of our trusted Viewers to tour and inspect
+            an existing property listing that isn’t listed on Dibby.
+          </p>
+          <h6>Enter Property URL</h6>
+          <input
+            placeholder='Property URL'
+            className='landing-send-a-viewer-inputs'
+          />
+          <h6>Enter Property Address</h6>
+          <input
+            placeholder='Address'
+            className='landing-send-a-viewer-inputs'
+          />
+          <input
+            placeholder='Apt, Unit, Floor, etc.'
+            className='landing-send-a-viewer-inputs'
+          />
+          <input placeholder='City' className='landing-send-a-viewer-inputs' />
+          <div className='landing-send-a-viewer-state-zip-inputs-box'>
+            <input
+              placeholder='Apt, Unit, Floor, etc.'
+              className='landing-send-a-viewer-inputs'
+            />
+            <input
+              placeholder='City'
+              className='landing-send-a-viewer-inputs'
+            />
+          </div>
+          <button
+            id='confirm-pay-send-a-viewer-button'
+            onClick={() => setShowConfirmAndPayPopup(true)}
+          >
+            Confirm and Pay
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SendAViewer;
