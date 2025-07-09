6538701802896bbee1d9ad3875725655d89ba92a
Brandon <bbeuttel@gmail.com>
2025-07-09 15:43:33 -0700

    updated import path

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/Navbar/index.jsx';
-import Footer from '../components/LandingPageSections/Footer/index.jsx';
+import Footer from '../components/Footer/index.jsx';
 import HeroSection from '../components/AboutPageSections/HeroSection/index.jsx';
 import WhoAreWe from '../components/AboutPageSections/WhoAreWeSection/index.jsx'
 import OurMission from '../components/AboutPageSections/OurMissionSection/index.jsx';
