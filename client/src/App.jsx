a36384bda72f84ba9b239b10318e9169f34b6e2a
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-12 14:21:46 -0400

    Cleaned up routes page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LandingPage from './pages/LandingPage.jsx';
 import PropertyListings from './pages/PropertyListings.jsx';
 import ProfilePage from './pages/ProfilePage.jsx';
-import AboutPage from './pages/AboutPage.jsx'
+import AboutPage from './pages/AboutPage.jsx';
 import PropertyDetails from './pages/PropertyDetails.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -10,10 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/profile' element={<ProfilePage />} />
-          <Route path='/about' element={<AboutPage />} />
         <Route path='/' element={<LandingPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/about' element={<AboutPage />} />
         <Route path='/property-listings' element={<PropertyListings />} />
         <Route path='/property-details' element={<PropertyDetails />} />
       </Routes>
