fbd910494c99fad1d65a46bf0dde36e9723fca97
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-15 15:04:05 -0400

    Getting other pages to work

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/doggy-figma/',
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
 });
