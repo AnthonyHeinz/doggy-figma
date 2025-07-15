213ee4371365ac98b5d80e255221e7ad3b5b4356
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-15 14:13:19 -0400

    Fixed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/doggy-figma/',
+  base: '/doggy_figma/',
   plugins: [react()],
 });
