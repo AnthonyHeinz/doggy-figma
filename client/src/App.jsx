4505c0a6981aa0579ce0968fff49c92f9b85675c
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-16 18:48:29 -0400

    Updated app.jsx to get rid of redundant route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,7 @@ function App() {
         <Route path='/profile' element={<ProfilePage />} />
         <Route path='/about' element={<AboutPage />} />
         <Route path='/property-listings' element={<PropertyListings />} />
-        <Route path='/property-details' element={<PropertyDetails />} />
-        <Route path='/property-details/:id' element={<PropertyDetails />} />
+        <Route path='/property-details/:id?' element={<PropertyDetails />} />
       </Routes>
     </BrowserRouter>
   );
