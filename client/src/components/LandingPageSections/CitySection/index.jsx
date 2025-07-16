1d3ea24b3fbd2fda9af4c5852736ef1ae09f719c
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-16 18:35:50 -0400

    Fixed city section css bugs

--- client/src/components/LandingPageSections/CitySection/index.jsx
+++ client/src/components/LandingPageSections/CitySection/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import search from '../../../assets/Search.png';
-import { useState } from 'react';
 import SendAViewer from '../SendAViewer/index.jsx';
 
 function CitySection() {
@@ -11,37 +10,37 @@ function CitySection() {
     <div className='city-section-wrapper'>
       <section className='city-section-container'>
         <div className='city-section-content'>
-          <h1 className='city-section-content-h1-text'>
+          <h1 className='city-section-h1-text'>
             We tour properties just for you.
           </h1>
-          <h6 className='city-section-content-h6-text'>
+          <h6 className='city-section-h6-text'>
             Found a home that looks promising? Whether it’s listed on Dibby or
             somewhere else, we’ll send a trusted Viewer to tour it for you and
             give a full, honest report so you can move with confidence.
           </h6>
           <div className='city-section-options'>
             <div>
-              <p className='city-section-content-p-text'>
+              <p className='city-section-p-text'>
                 Start browsing listings on Dibby
               </p>
               <div className='city-section-input-wrapper'>
                 <input
                   type='text'
                   placeholder='Enter city or ZIP Code'
-                  className='city-options-input'
+                  className='city-section-input'
                 />
-                <button className='overlay-button'>
+                <button className='city-section-overlay-button'>
                   <img src={search} alt='Search' />
                 </button>
               </div>
             </div>
-            <div className='or-separator'>OR</div>
+            <div className='city-section-or-separator'>OR</div>
             <div>
-              <p className='city-section-content-p-text'>
+              <p className='city-section-p-text'>
                 Send a Viewer to a property you’ve found
               </p>
               <button
-                className='viewer-button'
+                className='city-section-viewer-button'
                 onClick={() => setShowSendAViewerPopup(true)}
               >
                 Send a Viewer
@@ -50,8 +49,8 @@ function CitySection() {
           </div>
         </div>
       </section>
-      <div className='how-it-works-gradient-one'></div>
-      <div className='city-white-background'></div>
+      <div className='city-section-gradient-one'></div>
+      <div className='city-section-white-background'></div>
       <SendAViewer
         isOpen={showSendAViewerPopup}
         onClose={() => setShowSendAViewerPopup(false)}
