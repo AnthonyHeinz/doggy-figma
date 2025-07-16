affb65cfe86c6d2873c3704898349734bfea4761
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-16 19:57:43 -0400

    Created navbar drop down

--- client/src/components/Navbar/AccountDetailsDropDown/index.jsx
+++ client/src/components/Navbar/AccountDetailsDropDown/index.jsx
@@ -1,22 +1,25 @@
-import React from "react"
-import './styles.css'
+import React from 'react';
+import './styles.css';
 
-function AccountDetailsDropDown({ onClose }){
-return(
-    <div className="account-details-drop-down-container">
-        <div className="account-details-drop-down-content">
-            <button className="account-details-drop-down-option" onClick={() => console.log('Profile clicked')}>
-                Profile
-            </button>
-            <button className="account-details-drop-down-option" onClick={() => console.log('Settings clicked')}>
-                Settings
-            </button>
-            <button className="account-details-drop-down-option" onClick={() => console.log('Sign out clicked')}>
-                Sign Out
-            </button>
-        </div>
+function AccountDetailsDropDown() {
+  return (
+    <div className='account-details-drop-down-container'>
+      <div className='account-details-drop-down-content'>
+        <button
+          className='account-details-drop-down-option'
+          onClick={() => console.log('Profile clicked')}
+        >
+          My Account
+        </button>
+        <button
+          className='account-details-drop-down-option'
+          onClick={() => console.log('Sign out clicked')}
+        >
+          Sign Out
+        </button>
+      </div>
     </div>
-)
+  );
 }
 
-export default AccountDetailsDropDown
+export default AccountDetailsDropDown;
