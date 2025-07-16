f792adee4d42cec25ac17e0d7c3e07435530c267
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-07-16 19:47:17 -0400

    Fixed account details drown down init commit

---
+++ client/src/components/Navbar/AccountDetailsDropDown/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './styles.css'
+
+function AccountDetailsDropDown(){
+return(
+    <div className="account-details-drop-down-container">
+
+    </div>
+)
+}
+
+export default AccountDetailsDropDown
